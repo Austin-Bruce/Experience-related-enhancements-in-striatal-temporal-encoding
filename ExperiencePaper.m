2b7e184f82bd6a5ff9f640c33767a3ac26939783
Austin-Bruce <r.austin.bruce@protonmail.ch>
2021-10-19 12:07:56 -0500

    Add files via upload

---
+++ ExperiencePaper.m
@@ -0,0 +1,16 @@
+%% Austin Bruce / edited by Kumar Narayanan / Checked by Eric Emmons & Youngcho Kim  
+% 1/2/21
+close all
+clear all 
+
+tic; 
+%% Corrected clustering
+% loadRawData_v5; %sets up dataSt and neuronDB. Raw files needed. 
+LoadData %
+Figure2_Revision; % 
+Figure3_NeuronCounts; %Neuron by neuron generlized linear models
+Figure4_PCA;
+Figure5_Ensembles; %naive Bayes 
+Figure6_ErrorAnalysis; %GLMe's for neurons with error-related activity
+toc; 
+
